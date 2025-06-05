d0bee07c175263686af99a7d9c049eff82d46502
rudyibgoss <118284917+rudyibgoss@users.noreply.github.com>
2025-06-05 15:25:22 -0400

    added inline comments and update wsgi.py

--- transportation_Database/wsgi.py
+++ transportation_Database/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+import os #to interact with environment variables and system settings
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application #Django function to create a WSGI-compatible application
 
+#default settings module that Django will use to load configurations
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transportation_Database.settings')
 
+#the WSGI application object that the web server will use to serve your project
 application = get_wsgi_application()
