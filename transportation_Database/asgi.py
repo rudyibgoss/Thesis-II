fedbca6936fdbc52c175aad88c8cf763ef6ad53e
rudyibgoss <118284917+rudyibgoss@users.noreply.github.com>
2025-06-05 15:29:34 -0400

    added inline comments and update asgi.py

--- transportation_Database/asgi.py
+++ transportation_Database/asgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-import os
+import os #allows to interact with environment variables and system-level settings
 
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application #function that creates an ASGI-compliant Django application
 
+#tell Django which settings module to use
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transportation_Database.settings')
 
+#create the ASGI application instance that your ASGI server will use
+#this acts as the entry point for asynchronous communication
 application = get_asgi_application()
