e191e1154a92278f72b7f70b9e8f07097e43bbde
rudyibgoss <118284917+rudyibgoss@users.noreply.github.com>
2025-06-06 15:09:58 -0400

    added inline comments and update apps.py

--- transportation/apps.py
+++ transportation/apps.py
@@ -1,6 +1,10 @@
-from django.apps import AppConfig
+from django.apps import AppConfig #importing AppConfig to configure this app's settings
 
-
+#this class configures the 'transportation' app for the Django project
 class TransportationConfig(AppConfig):
+
+    #specifies the default type of primary key to use for models in this app
     default_auto_field = 'django.db.models.BigAutoField'
+
+    #the name of the app this configuration belongs to
     name = 'transportation'
